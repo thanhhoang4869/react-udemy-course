b541d8bb1a714fd35a801429545c457bd6027482
Thanh Hoang <thanhhoang4869@gmail.com>
2022-06-09 11:03:35 +0700

    handle input in form

--- form-pics/src/components/SearchBar/index.jsx
+++ form-pics/src/components/SearchBar/index.jsx
@@ -1,17 +1,52 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  // state = { password: "", message: "Password must be more than 4 character" };
+  state = { term: "" };
+
+  //to prevent undefined state in function scope
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <>
-        <div class="ui segment">
-          <div className="ui form">
-            <div class="field">
+        <div className="ui segment">
+          <form className="ui form">
+            <div className="field">
               <label>Image search</label>
-              <input type="text" />
+              <input
+                type="text"
+                value={this.state.term}
+                onChange={(e) => {
+                  this.setState({ term: e.target.value });
+                }}
+              />
+            </div>
+          </form>
+        </div>
+
+        {/* <div className="ui segment">
+          <div className="ui form">
+            <div className="field">
+              <label>Password</label>
+              <input
+                type="password"
+                value={this.state.password}
+                onChange={(e) => {
+                  this.setState({ password: e.target.value });
+                }}
+              />
+              <label>
+                {this.state.password.length < 5
+                  ? "Password must be more than 4 charater"
+                  : ""}
+              </label>
             </div>
           </div>
-        </div>
+        </div> */}
       </>
     );
   }
