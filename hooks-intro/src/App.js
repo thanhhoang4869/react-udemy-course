d00b6fc29dc9cdac059d271b0066cd7e7291b5b2
Thanh Hoang <thanhhoang4869@gmail.com>
2022-06-10 14:58:23 +0700

    open and show dropdown ls

--- hooks-intro/src/App.js
+++ hooks-intro/src/App.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
 
-const items = [
+const options = [
   {
-    title: "What is React?",
-    content: "React is a front end javascript framework",
+    label: "The Color Red",
+    value: "red",
   },
   {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
+    label: "The Color Green",
+    value: "green",
   },
   {
-    title: "How do you use React?",
-    content: "You use React by creating components",
+    label: "The Color Blue",
+    value: "blue",
   },
 ];
 
 function App() {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       <br />
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 }
