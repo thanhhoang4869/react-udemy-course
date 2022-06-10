8995dffa06deb88dfd2a1a32aa4b34f504f1dd6b
Thanh Hoang <thanhhoang4869@gmail.com>
2022-06-10 15:35:34 +0700

    finish translate and nav

--- hooks-intro/src/App.js
+++ hooks-intro/src/App.js
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite JS library among engineers",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components",
-//   },
-// ];
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 const options = [
   {
@@ -27,25 +31,35 @@ const options = [
     value: "green",
   },
   {
-    label: "The Color Blue",
+    label: "A Shade of Blue",
     value: "blue",
   },
 ];
 
-function App() {
+const App = () => {
   const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <br />
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
-}
-
+};
 export default App;
--- hooks-intro/src/components/Dropdown/index.jsx
+++ hooks-intro/src/components/Dropdown/index.jsx
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -23,7 +38,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
